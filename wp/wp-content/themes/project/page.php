27e9611810a45358daa916ea38c693f0a01e91ec
rin-jpg <rinzokun0105@gmail.com>
2022-01-12 17:49:48 +0900

    wp_headとwp_footerが2重で呼び出されていたのを修正

--- wp/wp-content/themes/project/page.php
+++ wp/wp-content/themes/project/page.php
@@ -3,12 +3,10 @@
 Template Name: 静的ページ
 */
 get_header();
-wp_head();
 
 while (have_posts()) : the_post();
 remove_filter('the_content', 'wpautop');
 the_content();
 endwhile;
 
-wp_footer();
 get_footer();
