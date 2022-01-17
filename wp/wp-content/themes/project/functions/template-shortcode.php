e6cf7ac35a9c8591ea5e17482ec0ce295a844966
rin-jpg <rinzokun0105@gmail.com>
2022-01-17 19:13:57 +0900

    cf7でもショートコードを使用可に

--- wp/wp-content/themes/project/functions/template-shortcode.php
+++ wp/wp-content/themes/project/functions/template-shortcode.php
@@ -6,6 +6,23 @@ function my_get_directory_assets() {
   return $path;
 }
 add_shortcode('get_directory_assets', 'my_get_directory_assets');
+// wpcf7_add_shortcode('get_directory_assets', 'my_get_directory_assets');
+
+// ショートコード: images
+function my_get_directory_images() {
+  $path = get_template_directory_uri() . '/assets/images/';
+  return $path;
+}
+add_shortcode('get_directory_images', 'my_get_directory_images');
+// wpcf7_add_shortcode('get_directory_images', 'my_get_directory_images');
+
+// ショートコード: media
+function my_get_directory_media() {
+  $path = get_template_directory_uri() . '/assets/media/';
+  return $path;
+}
+add_shortcode('get_directory_media', 'my_get_directory_media');
+// wpcf7_add_shortcode('get_directory_media', 'my_get_directory_media');
 
 // ショートコード: root
 function my_get_directory_root() {
@@ -13,6 +30,7 @@ function my_get_directory_root() {
   return $path;
 }
 add_shortcode('get_directory_root', 'my_get_directory_root');
+// wpcf7_add_shortcode('get_directory_root', 'my_get_directory_root');
 
 // srcsetでショートコードを使用可に
 add_filter( 'wp_kses_allowed_html', 'my_wp_kses_allowed_html', 10, 2 );
