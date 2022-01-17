19d6b0e2a93e36854477bde8619cf1056f2c3273
rin-jpg <rinzokun0105@gmail.com>
2022-01-17 19:12:18 +0900

    ファイル拡張子、ファイルサイズ取得用関数作成

--- wp/wp-content/themes/project/functions/template-acf.php
+++ wp/wp-content/themes/project/functions/template-acf.php
@@ -157,3 +157,35 @@ if ( ! function_exists( 'my_get_retina_url' ) ) {
     return $get_image;
   }
 }
+
+
+// ファイルの拡張子を取得
+if( ! function_exists( 'my_get_file_extension' ) ){
+  function my_get_file_extension($field_name, $sub = null){
+    if( $sub === true ){
+      $file_name = get_sub_field($field_name)['filename'];
+    }else{
+      $file_name = get_field($field_name)['filename'];
+    }
+    $file_extension = explode( '.', $file_name );
+    
+    // 配列の最後を取得
+    $last_str = count($file_extension) - 1;
+    return $file_extension[$last_str];
+  }
+}
+
+// ファイルサイズ取得
+if( ! function_exists( 'my_get_file_size' ) ){
+  function my_get_file_size($field_name, $sub = null){
+    if( $sub === true ){
+      $file_path = get_sub_field($field_name)['url'];
+    }else{
+      $file_path = get_field($field_name)['url'];
+    }
+    $file_directory_path = str_replace(esc_url(home_url('/'))."wp/", ABSPATH, $file_path); // ファイル URL をディレクトリパスへ変換
+    if(is_file($file_directory_path)){
+      return size_format(filesize($file_directory_path)); // ファイルサイズを表示
+    }
+  }
+}
