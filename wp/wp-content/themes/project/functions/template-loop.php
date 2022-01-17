3e28a3718254d7b8c358c5aecc96c7e8b1fd4cc2
rin-jpg <rinzokun0105@gmail.com>
2022-01-17 19:15:10 +0900

    年月アーカイブの表示

--- wp/wp-content/themes/project/functions/template-loop.php
+++ wp/wp-content/themes/project/functions/template-loop.php
@@ -23,3 +23,33 @@ function my_pre_get_posts( $query ) {
   }
 }
 add_action( 'pre_get_posts', 'my_pre_get_posts' );
+
+// 年月アーカイブの表示
+if ( ! function_exists( 'my_wp_get_archives' ) ) {
+  function my_wp_get_archives($args = array(), $before = null, $after = null) {
+    global $post;
+
+    $pattern = array( '/<li>/', '/<a href=\'((?:https?|ftp):\/\/[-_.!~*\'()a-zA-Z0-9;\/?:@&=+$,%#]+)\'>/', '/<\/a>&nbsp;\(([0-9]*)\)/' );
+    $replace = array( '<li class="l-side-list__col">', '<a href="$1">', '年</a></li>' );
+
+    $post_type = get_post_type();
+    $post_type = !empty( $post_type ) ? $post_type : get_post_type( $post );
+
+    $defaults = array(
+      'type' => 'yearly',
+      'format' => 'html',
+      'show_post_count' => 1,
+      'echo' => 0,
+      'post_type' => $post_type
+    );
+
+    $args = wp_parse_args( $args, $defaults );
+    $args = wp_get_archives( $args );
+
+    if ( !empty( $args ) ) {
+      return $before . preg_replace( $pattern, $replace, $args ) . $after;
+    } else {
+      return false;
+    }
+  }
+}
