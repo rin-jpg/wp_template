25613692388b0b9f64561e74ec55d84e7a93b698
rin-jpg <rinzokun0105@gmail.com>
2022-01-17 19:12:49 +0900

    固定ページのみビジュアルエディタ表示を無効

--- wp/wp-content/themes/project/functions/template-page.php
+++ wp/wp-content/themes/project/functions/template-page.php
@@ -32,4 +32,15 @@
 // // 本文の自動整形を無効化
 // remove_filter('the_content', 'wpautop');
 
-
+// 固定ページのみビジュアルエディタ表示を無効
+function disable_visual_editor_in_page(){
+    global $typenow;
+    if( $typenow == 'page' ){
+        add_filter('user_can_richedit', 'disable_visual_editor_filter');
+    }
+}
+function disable_visual_editor_filter(){
+    return false;
+}
+add_action( 'load-post.php', 'disable_visual_editor_in_page' );
+add_action( 'load-post-new.php', 'disable_visual_editor_in_page' );
